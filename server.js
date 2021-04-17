14ef77f3fdd4ddcd01250964b0abfdb4ebe1ffd9
Manohar Choudary <manoharchoudaryv@gmail.com>
2021-04-17 19:53:42 +0530

    mongodb connection

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 app.use(bodyParser.json());
 
@@ -10,9 +11,23 @@ app.listen(port,()=>{
     console.log("http://localhost:"+port);
 }); 
 
+mongoose.connect('mongodb://localhost/tejas_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(console.log("Db connected successfully")).
+catch((err)=>{
+    console.log(err);
+    process.abort();
+})
+
 app.set("view engine", "ejs");
 app.get("/",(req,res)=>{ 
     res.send("Hellow World");
 });
+
+
+
 
 require('./app/routes/test.routes')(app);
